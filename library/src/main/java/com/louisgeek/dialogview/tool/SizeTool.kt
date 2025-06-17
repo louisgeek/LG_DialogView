a1fc860f7dab032405859c12eceba31116d269d0
louisgeek <louisgeek@qq.com>
2025-06-17 09:32:43 +0800

    mod

---
+++ library/src/main/java/com/louisgeek/dialogview/tool/SizeTool.kt
@@ -0,0 +1,18 @@
+package com.louisgeek.dialogview.tool
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+/**
+ * Created by louisgeek on 2021/11/4.
+ */
+internal object SizeTool {
+    @JvmStatic
+    fun dp2px(px: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            px.toFloat(),
+            Resources.getSystem().displayMetrics
+        ).toInt()
+    }
+}
