105f03fc87224b52e38b70538d9a240cda826a94
louisgeek <louisgeek@qq.com>
2025-07-02 17:20:15 +0800

    mod

--- library/src/main/java/com/louisgeek/dialogview/DialogView.java
+++ library/src/main/java/com/louisgeek/dialogview/DialogView.java
@@ -178,6 +178,19 @@ public class DialogView extends DialogFragment {
         }
     }
 
+    private void updateDimAmount(float dimAmount) {
+        Dialog dialog = this.getDialog();
+        if (dialog != null && dialog.getWindow() != null) {
+            dialog.getWindow().setDimAmount(dimAmount); //0f 全透明 1f 全黑
+        }
+    }
+
+    private void updateLayoutSize(int width, int height) {
+        Dialog dialog = this.getDialog();
+        if (dialog != null && dialog.getWindow() != null) {
+            dialog.getWindow().setLayout(width, height); //任意一个为 0，可以实现事件穿透
+        }
+    }
 
     public View getContentView() {
         return mDialogViewConfig.getContentView();
